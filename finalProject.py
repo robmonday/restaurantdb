a327273d7c41fe4811825ebd821d3d4d7755bd40
Rob Monday <rob.monday@gmail.com>
2016-04-28 07:39:45 -0500

    Add login template page and point routing to it

--- finalProject.py
+++ finalProject.py
@@ -21,7 +21,7 @@ session = DBSession()
 def showLogin():
 	state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
 	login_session['state'] = state
-	return "The current session state is %s" %login_session['state']
+	return render_template('login.html')
 
 @app.route('/restaurants/JSON/')
 def restaurantsJSON():
